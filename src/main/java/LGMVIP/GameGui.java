6abe0dd0283c0ead6fbdf443cb33ce0beeb6ba07
pranish <you@example.com>
2023-08-30 01:21:31 +1000

    Everything completely done

--- src/main/java/LGMVIP/GameGui.java
+++ src/main/java/LGMVIP/GameGui.java
@@ -1,2 +1,86 @@
-package LGMVIP;public class GameGui {
+package LGMVIP;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GameGui {
+
+    static JLabel player;
+    JButton button1, button2, button3, button4, button5, button6, button7, button8, button9;
+    JButton[] buttons;
+
+    void createGui() {
+        //creating a frame for the game
+        JFrame frame = new JFrame("Tic Tac Toe");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500, 600);
+        frame.setLocation(500, 200);
+        frame.setLayout(null);
+
+
+        //panel for our player screen
+        JPanel playerPanel = new JPanel();
+        playerPanel.setLayout(null);
+        playerPanel.setBounds(0, 0, 500, 100);
+        playerPanel.setBackground(Color.lightGray);
+        frame.add(playerPanel);
+
+        //JLabel for player
+        player = new JLabel();
+        player.setText("       Tic-Tac-Toe ");
+        player.setBounds(15, 15, 500, 50);
+        player.setFont(new Font("Arial", Font.BOLD, 50));
+        player.setForeground(new Color(0x8A2BE2	));
+        playerPanel.add(player);
+
+
+        // panel for our game
+        JPanel gamePanel = new JPanel();
+        gamePanel.setLayout(new GridLayout(3, 3));
+        frame.add(gamePanel);
+        gamePanel.setBounds(0, 100, 500, 500);
+
+
+        button1 = new JButton();
+        button2 = new JButton();
+        button3 = new JButton();
+        button4 = new JButton();
+        button5 = new JButton();
+        button6 = new JButton();
+        button7 = new JButton();
+        button8 = new JButton();
+        button9 = new JButton();
+
+        //adding buttons to an array
+        buttons = new JButton[]{button1, button2, button3, button4, button5, button6, button7, button8, button9};
+
+        //setting button names
+        button1.setName("button1");
+        button2.setName("button2");
+        button3.setName("button3");
+        button4.setName("button4");
+        button5.setName("button5");
+        button6.setName("button6");
+        button7.setName("button7");
+        button8.setName("button8");
+        button9.setName("button9");
+
+        //fonts for buttons
+        //adding buttons to the frame
+        //adding action listener to the buttons
+        for (JButton button : buttons) {
+            button.setFont(new Font("Arial", Font.BOLD, 100));
+            gamePanel.add(button);
+            button.addActionListener(new GameLogic());
+            button.setFocusable(false);
+        }
+
+
+        frame.setVisible(true);// making the frames visible
+
+    }
+
+
 }
+
+
--- src/main/java/LGMVIP/Main.java
+++ src/main/java/LGMVIP/Main.java
@@ -1,126 +1,14 @@
 package LGMVIP;
 
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+public class Main {
 
-public class GUI implements ActionListener {
-    private JTextArea IODisplay;
+    public static void main(String[] args) throws InterruptedException {
+        GameGui gameGui = new GameGui();// creating an object of the GameGui class
 
-    public GUI() {
-        // Create the JFrame and set its properties
-        JFrame frame1 = new JFrame();
-        frame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame1.setTitle("AIT BANK");
-        frame1.setSize(400, 400);
-        frame1.setLayout(null); // Set layout to null
-
-        // Create components
-        JLabel greeting = new JLabel("Welcome to AIT Bank ATM");
-        greeting.setBounds(0, 0, 400, 20); // Set bounds for the greeting label
-        IODisplay = new JTextArea(7, 20);
-
-        JButton button1 = new JButton("1");
-        JButton button2 = new JButton("2");
-        JButton button3 = new JButton("3");
-        JButton button4 = new JButton("4");
-        JButton button5 = new JButton("5");
-        JButton button6 = new JButton("6");
-        JButton button7 = new JButton("7");
-        JButton button8 = new JButton("8");
-        JButton button9 = new JButton("9");
-        JButton button0 = new JButton("0");
-        JButton buttonPoint = new JButton(".");
-        JButton buttonClear = new JButton("Clear");
-
-        JButton buttonWithdraw = new JButton("Withdraw");
-        JButton buttonDeposit = new JButton("Deposit");
-        JButton buttonInquiry = new JButton("Balance Inquiry");
-        JButton buttonHelp = new JButton("Help");
-
-        JButton buttonCancel = new JButton("Cancel");
-        JButton buttonEnter = new JButton("Enter");
-
-        // Set ActionListeners for buttons
-        button1.addActionListener(this);
-        button2.addActionListener(this);
-        button3.addActionListener(this);
-        button4.addActionListener(this);
-        button5.addActionListener(this);
-        button6.addActionListener(this);
-        button7.addActionListener(this);
-        button8.addActionListener(this);
-        button9.addActionListener(this);
-        button0.addActionListener(this);
-        buttonPoint.addActionListener(this);
-        buttonClear.addActionListener(this);
-        buttonWithdraw.addActionListener(this);
-        buttonDeposit.addActionListener(this);
-        buttonInquiry.addActionListener(this);
-        buttonHelp.addActionListener(this);
-        buttonCancel.addActionListener(this);
-        buttonEnter.addActionListener(this);
-
-        // Create panels to organize components
-        JPanel displayPanel = new JPanel(new GridLayout(2, 1));
-        displayPanel.setBounds(0, 20, 400, 100); // Set bounds for the displayPanel
-        displayPanel.add(greeting);
-        displayPanel.add(IODisplay);
-
-        JPanel numberPanel = new JPanel(new GridLayout(4, 3));
-        numberPanel.setBounds(0, 120, 300, 180); // Set bounds for the numberPanel
-        numberPanel.add(button1);
-        numberPanel.add(button2);
-        numberPanel.add(button3);
-        numberPanel.add(button4);
-        numberPanel.add(button5);
-        numberPanel.add(button6);
-        numberPanel.add(button7);
-        numberPanel.add(button8);
-        numberPanel.add(button9);
-        numberPanel.add(button0);
-        numberPanel.add(buttonPoint);
-        numberPanel.add(buttonClear);
-
-        JPanel actionPanel = new JPanel(new GridLayout(4, 1, 5, 0));
-        actionPanel.setBounds(300, 120, 100, 180); // Set bounds for the actionPanel
-        actionPanel.add(buttonWithdraw);
-        actionPanel.add(buttonDeposit);
-        actionPanel.add(buttonInquiry);
-        actionPanel.add(buttonHelp);
-
-        JPanel controlPanel = new JPanel(new GridLayout(1, 2));
-        controlPanel.setBounds(0, 300, 400, 100); // Set bounds for the controlPanel
-        controlPanel.add(buttonCancel);
-        controlPanel.add(buttonEnter);
-
-        // Add panels to the frame
-        frame1.add(displayPanel);
-        frame1.add(numberPanel);
-        frame1.add(actionPanel);
-        frame1.add(controlPanel);
-
-        // Set frame visibility
-        frame1.setVisible(true);
+        GameLogic gameLogic = new GameLogic();// creating an object of the GameLogic class
+        gameGui.createGui(); // creating the gui for the game
+        Thread.sleep(500);
+        GameGui.player.setText("      Player X turn"); // setting the player label to player X turn
     }
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        // Handle button clicks here
-        if (e.getSource() instanceof JButton) {
-            JButton clickedButton = (JButton) e.getSource();
-            String buttonText = clickedButton.getText();
-
-            // Store the clicked button's text in the public variable
-            String lastClickedButton = buttonText;
-
-            // Append the clicked button's text to the text area
-            IODisplay.append(buttonText);
-        }
-    }
-
-    public static void main(String[] args) {
-        new GUI();
-    }
 }
